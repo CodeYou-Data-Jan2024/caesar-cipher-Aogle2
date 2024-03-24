9a1faad3b0b1a3eb29591715eb02ba8bfcd506e9
Aaron Ogle <156380776+Aogle2@users.noreply.github.com>
2024-03-24 11:03:03 -0400

    another try after sleep

--- cipher.py
+++ cipher.py
@@ -1,11 +1,12 @@
 # add your code here
 def cipher(text):
     result = ""
+    shift = 5
     for char in text:
         if char.isalpha():
             base = ord('A') if char.isupper() else ord('a')
-            shifted = chr((ord(char) - base + 5) % 26 + base)
+            shifted = chr((ord(char) - base + shift) % 26 + base)
             result += shifted
         else:
             result += char
-    print('The encrypted sentence is: '+result)
+    print('The encrypted text is: ' + result.lower())
